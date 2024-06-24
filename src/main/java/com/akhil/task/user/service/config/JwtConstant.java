05cdc5d24a0c97b48a4526a59ecefe7b68c5f19d
canikhil12 <75828601+canikhil12@users.noreply.github.com>
2024-06-23 22:26:44 -0400

    task

---
+++ src/main/java/com/akhil/task/user/service/config/JwtConstant.java
@@ -0,0 +1,9 @@
+package com.akhil.task.user.service.config;
+
+import com.zaxxer.hikari.pool.HikariProxyResultSet;
+
+public class JwtConstant {
+public static final String SECRET = "SecretKeyToGenJWTs";
+public static final String JWT_HEADER = "Authorization";
+
+}
